3ec7bf3051660ac6548dfd93a3e01ec9e5f6a7f3
AdriDev <adriDev1993@gmail.com>
2023-12-15 08:33:33 +0100

    Sidebar and routing created

---
+++ src/app/countries/countries-routing.module.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  {
+
+  }
+];
+
+@NgModule({
+  imports: [],
+  exports: [],
+})
+export class CountriesRoutingModule { }
